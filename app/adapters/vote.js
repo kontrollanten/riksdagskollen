a240f1cbb55291b689a5bbbc308da1f60d0f6830
kontrollanten <alexander@width.se>
2019-01-25 16:58:18 +0100

    votes: hantera XML respons
    fixes #1

--- app/adapters/vote.js
+++ app/adapters/vote.js
@@ -1,4 +1,5 @@
 import DS from 'ember-data';
+import {xml2json} from 'xml-js';
 
 export default DS.RESTAdapter.extend({
 
@@ -6,9 +7,24 @@ export default DS.RESTAdapter.extend({
 
   namespace: 'voteringlista',
 
+  ajaxOptions: function(url, type, options) {
+    const hash = DS.RESTAdapter.prototype.ajaxOptions.call(this, url, 'GET', options);
+    hash.dataType = 'xml';
+    return hash;
+  },
+
+  handleResponse(status, headers, body) {
+    const xml = new XMLSerializer().serializeToString(body);
+
+    return JSON.parse(xml2json(xml, { compact: true,  textFn: (value, parentElem) => {
+      const keyNo = Object.keys(parentElem._parent).length;
+      const keyName = Object.keys(parentElem._parent)[keyNo - 1];
+      parentElem._parent[keyName] = value;
+    }}));
+  },
+
   urlForQuery(query) {
     let baseUrl = this.buildURL();
-    return `${baseUrl}/?sz=1000&utformat=json&iid=${query.memberId}`
+    return `${baseUrl}/?sz=1000&utformat=xml&iid=${query.memberId}`
   }
-
 });
