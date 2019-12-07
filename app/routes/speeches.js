d82865f012019162f53196690e8e0be6f82a808f
kontrollanten <stambanan@pm.me>
2019-12-07 19:21:24 +0100

    fix: länkar till röster och anföranden

--- app/routes/speeches.js
+++ app/routes/speeches.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params, transition) {
-    return this.get('store').query('speech', {memberId: transition.params.member.member_id});
+    return this.get('store').query('speech', {memberId: transition.to.parent.params.member_id});
   }
 });
--- app/routes/votes.js
+++ app/routes/votes.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model(params, transition) {
-    return this.get('store').query('vote', {memberId: transition.params.member.member_id}).then(data => {
+    return this.get('store').query('vote', {memberId: transition.to.parent.params.member_id}).then(data => {
       return data.sortBy('date').reverse();
     });
   }
